6e976c2adee0565754947ba167ea1f5a6161a0bf
Andrey <lislmaka@>
2025-11-29 14:16:12 +0300

    Создание тестовой модели
    Создание тестовой модели и наполнение ее данными
    при помощи factory-boy

--- services/backend/apps/testapp/models.py
+++ services/backend/apps/testapp/models.py
@@ -7,6 +7,28 @@ from django.db import models
 class Project(models.Model):
     """Docstring for Project."""
 
+    title = models.CharField(
+        max_length=250,
+        null=False,
+        blank=False,
+        verbose_name="Заголовок",
+    )
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        """Docstring for __str__.
+
+        :param self: Description
+        :return: Description
+        :rtype: str
+        """
+        return self.title
+
+
+class Comments(models.Model):
+    """Docstring for Project."""
+
     title = models.CharField(max_length=250, null=False, blank=False)
     description = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
