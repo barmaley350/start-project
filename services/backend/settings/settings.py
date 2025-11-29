2dbdbe4f2e3b96d662b42093be5bf80c4d807a87
Andrey <lislmaka@>
2025-11-29 17:33:30 +0300

    Настройка подключения в backend
    Была неправильно указана переменная окружения

--- services/backend/settings/settings.py
+++ services/backend/settings/settings.py
@@ -99,7 +99,7 @@ elif os.environ.get("DATABASE_TYPE") == "postgresql":
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_NAME"),
+            "NAME": os.environ.get("POSTGRES_DB"),
             "USER": os.environ.get("POSTGRES_USER"),
             "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
             "HOST": os.environ.get("POSTGRES_HOST"),
