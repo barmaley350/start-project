c80fe140fdd6ce6dcc5eba733beefbc75d7ea4fc
Andrey <lsmart@atomicmail.io>
2026-01-01 13:38:32 +0300

    Изменил queryset для Project

--- services/backend/apps/testapp/views.py
+++ services/backend/apps/testapp/views.py
@@ -1,9 +1,9 @@
 """Docstring for services.backend.apps.testapp.views."""
 
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 from rest_framework import generics
 
-from apps.testapp.models import Project
+from apps.testapp.models import Comment, Project
 
 from .serializers import ProjectSerializer
 
@@ -12,5 +12,12 @@ class ProjectList(generics.ListCreateAPIView):
     """Docstring for ProjectList."""
 
     # queryset = Project.objects.prefetch_related("comments").all()  # noqa: ERA001
-    queryset = Project.objects.annotate(comments_count=Count("comments")).all()
+    # queryset = Project.objects.annotate(comments_count=Count("comments")).all()  # noqa: E501, ERA001
+    queryset = (
+        Project.objects.select_related("owner")
+        .prefetch_related(
+            Prefetch("comments", queryset=Comment.objects.select_related("owner"))
+        )
+        .annotate(comments_count=Count("comments"))
+    )
     serializer_class = ProjectSerializer
