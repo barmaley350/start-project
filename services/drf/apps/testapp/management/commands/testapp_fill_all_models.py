8c53bbfffdcef04a59f4132a5166457d308c5080
Andrey Frolov <darmaley40@yandex.ru>
2026-01-13 16:00:49 +0300

    Изменил команду testapp_fill_all_models.py
    Более быстрое заполнение моделей demo данными

--- services/drf/apps/testapp/management/commands/testapp_fill_all_models.py
+++ services/drf/apps/testapp/management/commands/testapp_fill_all_models.py
@@ -1,10 +1,12 @@
 """Docstring для testapp_fill_all_models."""
 
 import random
+import time
 from typing import Any
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandParser
+from django.db import transaction
 
 from apps.testapp.factories import (
     CommentFactory,
@@ -47,10 +49,11 @@ class Command(BaseCommand):
         users = User.objects.count()
 
         self.output_text("")
-        self.output_text(f"Кол-во проектов {projects}")
-        self.output_text(f"Кол-во тегов {tags}")
-        self.output_text(f"Кол-во коментариев {comments}")
-        self.output_text(f"Кол-во пользователей {users}")
+        self.output_text("Общая статистика", "notice")
+        self.output_text(f"\t Кол-во проектов {projects}")
+        self.output_text(f"\t Кол-во тегов {tags}")
+        self.output_text(f"\t Кол-во коментариев {comments}")
+        self.output_text(f"\t Кол-во пользователей {users}")
 
     def clear_data(self, **options: dict[str:Any]) -> None:
         """Docstring для clear_data.
@@ -63,25 +66,27 @@ class Command(BaseCommand):
             Tag.objects.all().delete()
             User.objects.filter(pk__gt=1).delete()
 
-    def output_text(self, text: str, output_type: str = "success") -> None:
-        """Вывод текства в консоль.
+    def output_text(self, text: str, output_type: str | None = None) -> None:
+        """_summary_.
 
-        :param text: Текст для вывода
+        :param text: _description_
         :type text: str
-        :param output_type: Тип вывода, defaults to "success"
-        :type output_type: str, optional
+        :param output_type: _description_, defaults to None
+        :type output_type: str | None, optional
         """
         match output_type:
-            case "seccuss":
+            case "notice":
+                self.stdout.write(f"\033[94m{text}\033[0m")
+            case "success":
                 self.stdout.write(self.style.SUCCESS(text))
             case "error":
                 self.stdout.write(self.style.ERROR(text))
             case _:
-                self.stdout.write(self.style.SUCCESS(text))
+                self.stdout.write(text)
 
-    def output_process(self) -> None:
+    def output_process(self, idx: int) -> None:
         """Docstring для output_process."""
-        print(".", end="")  # noqa: T201
+        print(idx, end="\r")  # noqa: T201
 
     def filling_project_models(self, **options: dict[str:Any]) -> None:
         """Заполнение моделей данными.
@@ -93,20 +98,58 @@ class Command(BaseCommand):
 
         self.clear_data(**options)
 
-        projects = ProjectFactory.create_batch(count)
-        tags = TagFactory.create_batch(10)
         users = UserFactory.create_batch(10)
+        tags = TagFactory.create_batch(10)
 
-        for project in projects:
-            project_author = random.choice(users)  # noqa: S311
-            project.owner = project_author
-            project.save()
+        self.output_text(f"Создаем проекты - {count} шт. Ожидайте...", "notice")
+        project_data = []
+        for idx in range(count):
+            self.output_process(f"\t Создано {idx + 1} проектов из {count}")
+            project = ProjectFactory.build(
+                owner=random.choice(users)  # noqa: S311
+            )
+            project.id = None
+            project_data.append(project)
+        self.output_text("")
+        self.output_text(f"\t Создано проектов - {count}")
 
-            project.tags.add(*random.choices(tags, k=random.randint(3, 6)))  # noqa: S311
+        with transaction.atomic():
+            created_projects = Project.objects.bulk_create(project_data)
 
-            for _ in range(random.randint(5, 20)):  # noqa: S311
-                CommentFactory.create(project=project, owner=random.choice(users))  # noqa: S311
-            self.output_process()
+        min_comments = 4
+        max_comments = 10
+        min_tags = 3
+        max_tags = 6
+
+        self.output_text(
+            f"""Создаем коментарии. Min = {count * min_comments}, Max = {count * max_comments}. Ожидайте...""",  # noqa: E501
+            "notice",
+        )
+        all_comments = []
+        count_comments = 0
+        for idx, project in enumerate(created_projects):
+            selected_tags = random.choices(tags, k=random.randint(min_tags, max_tags))  # noqa: S311
+            project.tags.set(selected_tags)
+
+            num_comments = random.randint(min_comments, max_comments)  # noqa: S311
+            comment_users = random.sample(users, k=num_comments)
+            count_comments += num_comments
+
+            comments_for_project = [
+                CommentFactory.build(
+                    project=project,
+                    owner=user,
+                )
+                for user in comment_users
+            ]
+            all_comments.extend(comments_for_project)
+            self.output_process(
+                f"\t Обработано {idx + 1} проектов из {count}. Создано коментариев {count_comments}"  # noqa: E501
+            )
+
+        Comment.objects.bulk_create(all_comments)
+        self.output_text("")
+        self.output_text(f"\t Создано комментариев - {count_comments}")
 
     def handle(self, *args: list[Any], **options: dict[str:Any]) -> None:
         """Docstring для handle.
@@ -117,6 +160,13 @@ class Command(BaseCommand):
         :param options: Описание
         :type options: dict[str: Any]
         """
-        self.output_text(f"Создаем проекты - {options.get('count')} шт. Ожидайте...")
+        start_time = time.time()
         self.filling_project_models(*args, **options)
         self.print_stat()
+        end_time = time.time()
+
+        execution_time = end_time - start_time
+        self.output_text("")
+        self.output_text(
+            f"Добавление данных заняло {execution_time:.4f} сек.", "notice"
+        )
