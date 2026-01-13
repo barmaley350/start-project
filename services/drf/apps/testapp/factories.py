a1f5616a6c7a865a279ec563d143fd45ded5f729
Andrey Frolov <darmaley40@yandex.ru>
2026-01-13 15:58:22 +0300

    Изменил factories.py

--- services/drf/apps/testapp/factories.py
+++ services/drf/apps/testapp/factories.py
@@ -1,5 +1,7 @@
 """Docstring for services.backend.apps.testapp.factories."""
 
+import random
+
 import factory
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
@@ -36,8 +38,48 @@ class ProjectFactory(factory.django.DjangoModelFactory):
 
     title = factory.Faker("text", max_nb_chars=100)
     description = factory.Faker("text", max_nb_chars=500)
-    owner = factory.LazyAttribute(lambda _: User.objects.first())
+    # owner = None  # noqa: ERA001
+
+    # owner = factory.LazyAttribute(lambda _: User.objects.first()) # noqa: ERA001
     # owner = factory.SubFactory(UserFactory)  # noqa: ERA001
+
+    # @factory.post_generation
+    # def tags(
+    #     self,
+    #     create: any,
+    #     extracted: any,
+    #     **kwargs: dict[str, any],  # noqa: ARG002
+    # ) -> None:
+    #     """_summary_.
+
+    #     :param create: _description_
+    #     :type create: any
+    #     :param extracted: _description_
+    #     :type extracted: any | None
+    #     """
+    #     if not create:
+    #         return
+    #     if extracted:
+    #         self.tags.add(*extracted)
+
+    # @factory.post_generation
+    # def comments(
+    #     self,
+    #     create: any,
+    #     extracted: any,
+    #     **kwargs: dict[str, any],  # noqa: ARG002
+    # ) -> None:
+    #     """_summary_.
+
+    #     :param create: _description_
+    #     :type create: any
+    #     :param extracted: _description_
+    #     :type extracted: any
+    #     """
+    #     if not create:
+    #         return
+    #     count = extracted or 10
+    #     CommentFactory.create_batch(count, project=self)
 
 
 class CommentFactory(factory.django.DjangoModelFactory):
