0b35eb948cfb3d00d43f556b8e063f4e5f5892a5
Andrey Frolov <darmaley40@yandex.ru>
2026-01-13 18:26:20 +0300

    Изменил locust

--- services/drf/tests/external/locust.py
+++ services/drf/tests/external/locust.py
@@ -9,11 +9,29 @@ class ApiProject(HttpUser):
     # Время ожидания между запросами (в секундах)
     wait_time = between(1, 3)
 
-    @task(1)
-    def get_projects(self) -> None:
+    @task(5)
+    def get_projects_api(self) -> None:
         """Docstring для get_projects.
 
         :param self: Описание
         """
         headers = {}
         self.client.get("/api/v1/project/", headers=headers)
+
+    @task(5)
+    def get_projects(self) -> None:
+        """Docstring для get_projects.
+
+        :param self: Описание
+        """
+        headers = {}
+        self.client.get("/projects/", headers=headers)
+
+    @task(1)
+    def get_main(self) -> None:
+        """Docstring для get_main.
+
+        :param self: Описание
+        """
+        headers = {}
+        self.client.get("/", headers=headers)
