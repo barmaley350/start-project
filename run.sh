8ce8b134190b5c57320958a888e6e8124cac7134
Andrey Frolov <darmaley40@yandex.ru>
2026-02-13 12:02:28 +0300

    Изменил логику работы run.sh
    Добавил возможность создания пользовательских
    команд через файл run_commands.txt

--- run.sh
+++ run.sh
@@ -1,400 +1,245 @@
 #!/bin/bash
 
-# colors
+# Скрипт run.sh служит для упрощенного запуска повседневных команд, которые запускаются при работе с проектом
+# Команды делятся на 2 группы:
+#   1. Базовые команды 
+#   2. Пользовательские команды
+# Пользовательские команды берутся из файла run_commands.txt, который находится в корне проекта
+# Формат пользовательской команды:
+# --begin
+# Псевдоним
+# Команда
+# Описание что делает данная команда
+# --end
+# Все пользовательские команды должны быть заключены в блоки --begin --end
+# Все пользовательские команды запускаются относительно корня проекта
+
+
+# Определение цвета
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+NC='\033[0m' 
 
-PATH_TO_BACKEND_DJANGO="/services/drf"
-PATH_TO_BACKEND_FASTAPI="/services/fastapi"
-PATH_TO_BACKEND_DOCS="/services/drf/apps/sphinx_docs/docs"
+# Файл с пользовательскими командами
+USER_COMMAND_FILE="run_commands.txt"
 
-# Абсолютный путь к каталогу скрипта
+# Получение параметров командной строки
+USER_INPUT="$@"
+USER_COMMAND="$1"
+USER_PARAMS="$2"
+
+# Определение абсолютного пути к каталогу скрипта
 SCRIPT_DIR=$(cd -P "$(dirname -- "$0")" && pwd -P)
 
 # Извлекаем только имя папки из полного пути
 FOLDER_NAME=$(basename "$SCRIPT_DIR")
 
-# text success
-print_text_green() { 
-    echo -en "${GREEN} \u2714 $1${NC}" 
-}
-# text error
-print_text_red() {
-    echo -en "${RED} \u2718 $1${NC}"
-}
-# text yellow
-print_text_yellow() {
-    echo -en "${YELLOW}$1${NC}"
-}
-# text blue
-print_text_blue() {
-    echo -en "${BLUE}$1${NC}"
-}
-# text white
+# Вывод текста обычным, белым, цветом
 print_text_white() {
     echo -en "$1"
 }
-print_text_block() {
-    case $1 in
-        success)
-            line
-            print_text_green "$2 \n" 
-            line
-            ;;
-        error)
-            line
-            print_text_red "$2 \n"
-            line
-            ;;
-    esac
+
+# Вывод текста красным цветом  \u2718 
+print_text_red() {
+    echo -en "${RED}$1${NC}"
 }
-# lines
+
+# Вывод текста зеленым цветом \u2714 
+print_text_green() { 
+    echo -en "${GREEN}$1${NC}" 
+}
+
+# Вывод текста синим цветом
+print_text_blue() {
+    echo -en "${BLUE}$1${NC}"
+}
+
+# Вывод текста желтым цветом
+print_text_yellow() {
+    echo -en "${YELLOW}$1${NC}"
+}
+
+# Вывод линии на всю ширину экрана
 line() {
     cols=$(tput cols)
     for ((i=1; i<=cols; i++)); do echo -en "\u2500"; done
     echo -e ""
 }
-line2() {
-    cols=$(tput cols)
-    for ((i=1; i<=cols; i++)); do echo -en "\u2500"; done
-    echo -e ""
-}
 
-# help 
-help() {
-    
+# Вывод заголовка
+print_header() {
     line
     print_text_yellow "Справка по доступным командам \u00ABrun.sh\u00BB\n"
-    print_text_white "./run.sh [command] [command params]\n"
-    print_text_white "[command] \u2501 Основная команда либо команда быстрого доступа\n"
-    print_text_white "[command params] \u2501 Любые параметы которые принимает эта комманда\n"
-    line2    
-    print_text_white "[command] \u2501 Основные команды \n"
-    print_text_yellow "help | empty"
-    print_text_white " \u2501 Вывести справку \n"
-    print_text_yellow "m | manage [params]"
-    print_text_white " \u2501 Запустить manage.py в backend контейнере \n"    
-    print_text_yellow "p | pytest [params]"
-    print_text_white " \u2501 Запустить pytest в backend контейнере \n"    
-    print_text_yellow "shell"
-    print_text_white " \u2501 Запустить /bin/bash в backend контейнере для ручного выполнения команд\n" 
-    line2 
-    print_text_white "[command] - Команды быстрого доступа\n"
-    print_text_yellow "m1 | makemigrations"
-    print_text_white " \u2501 Создать миграции django (mange.py makemigrations) \n"     
-    print_text_yellow "m2 | migrate"
-    print_text_white " \u2501 Применить миграции django (manage.py migrate) \n"         
-    print_text_yellow "sd | sphinx_doc"
-    print_text_white " \u2501 Сгенерировать Sphinx документацию \n" 
-    print_text_yellow "rc | ruffcheck [all|drf|fastapi]"
-    print_text_white " \u2501 Запустить ruff check для drf или fastapi или all (default) для того и другого\n"     
-    print_text_yellow "rf | ruffformat [all|drf|fastapi]"
-    print_text_white " \u2501 Запустить ruff format для drf или fastapi или all (default) для того и другого\n"   
-    print_text_yellow "ra | ruff [all|drf|fastapi]"
-    print_text_white " \u2501 Запустить ruff check и ruff format для drf или fastapi или all (default) для того и другого\n"       
-    print_text_yellow "fc | commit"
-    print_text_white " \u2501 Запустить gendoc, ruff check, ruff format и pytest \n"      
-    print_text_yellow "da | create_admin"
-    print_text_white " \u2501 Создать django admin createsuperuser \n"  
-    print_text_yellow "ca | create_apps [app_name]"
-    print_text_white " \u2501 Создать новое приложение django \n"  
-    line2 
-    print_text_white "[command] - Персональные команды\n"
-    print_text_yellow "g1"
-    print_text_white " \u2501 git checkout main & git merge dev & \n git push gitlab dev & git push github dev & git push gitverse dev & \n git push gitlab main & git push github main & git push gitverse main\n" 
-    print_text_yellow "lbd"
-    print_text_white " \u2501 (backend/django) find ./apps -name "*.py" | xargs uv run pylint --rcfile=.pylintrc\n" 
-    print_text_yellow "lbf"
-    print_text_white " \u2501 (backend/fastapi) find ./app -name "*.py" | xargs uv run pylint --rcfile=.pylintrc\n"     
-    echo -e ""
+    print_text_white "./run.sh [command]\n"
+    line 
+    print_text_yellow "Основные команды\n"
+    line
+    print_text_green "mdrf [params]"
+    print_text_white " \u2501 Запустить manage.py в backend/drf контейнере \n"   
+    print_text_white "  В качестве [params] можно использовать, припример, makemigrations, migrate, createsuperuser и т.д. \n" 
+    print_text_white "  Полный перечень параметров можно получить запустив команду без параметров \n" 
+    print_text_green "tdrf [params]"
+    print_text_white " \u2501 Запустить pytest в backend/drf контейнере\n"      
+    print_text_green "sdrf"
+    print_text_white " \u2501 Запустить /bin/bash в backend/drf контейнере для ручного выполнения команд\n"  
+    print_text_green "sfast"
+    print_text_white " \u2501 Запустить /bin/bash в backend/fastapi контейнере для ручного выполнения команд\n"               
+    line  
 }
-# pytest
-command_pytest() {
-    docker exec -it ${FOLDER_NAME}-service.drf-1 uv run pytest "$@"
+
+# Основная функция которая читает файл с пользовательскими командами
+read_file() {
+    in_block=false
+    line_counter=0  
+    
+    print_text_yellow "Команды из файла \u00AB$USER_COMMAND_FILE\u00BB\n"; line
+
+    while IFS= read -r line; do
+        if [[ "$line" == "--begin" ]]; then
+            in_block=true
+            line_counter=0 
+            continue
+        fi
+
+        if [[ "$line" == "--end" ]]; then
+            in_block=false
+            line_counter=0  
+            continue
+        fi
+
+        if $in_block; then
+            ((line_counter++)) 
+
+            if [[ $line_counter -eq 1 ]]; then
+                print_text_blue "$line"
+            elif [[ $line_counter -eq 2 ]]; then
+                print_text_white " \u2501 $line \n"            
+            elif [[ $line_counter -ge 3 ]]; then
+                print_text_white "  $line \n"
+            fi
+        fi
+    done < "$SCRIPT_DIR/$USER_COMMAND_FILE"    
+    line
 }
+
+# Запуск пользовательской команды
+start_user_command() {
+    in_block=false
+    line_counter=0  
+    is_start=false
+    
+    while IFS= read -r line; do
+        if [[ "$line" == "--begin" ]]; then
+            in_block=true
+            line_counter=0 
+            continue
+        fi
+
+        if [[ "$line" == "--end" ]]; then
+            in_block=false
+            line_counter=0  
+            continue
+        fi
+
+        if $in_block; then
+            ((line_counter++)) 
+
+            if [[ $line_counter -eq 1 ]]; then
+                if [[ "$line" == "$USER_COMMAND" ]]; then
+                    is_start=true
+                else
+                    is_start=false
+                fi
+            elif [[ $line_counter -eq 2 ]]; then
+                if [[ $is_start == true ]]; then
+                    start_command $line
+                fi            
+            fi
+        fi
+    done < "$SCRIPT_DIR/$USER_COMMAND_FILE"    
+}
+
+# Проверка статуса выполнения команды
+check_command_run_status() {
+    command_run_status="$@"
+
+    if [ $command_run_status -ne 0 ]; then
+        line; print_text_red "\u2718 Ошибка выполнения комманды \u00AB$USER_INPUT\u00BB\n"; line
+        exit $command_run_status
+    fi
+    line; print_text_green "\u2714 Команда \u00AB$USER_INPUT\u00BB выполнилась успешно\n"; line
+
+    exit 0    
+} 
+# Непосредственный запуск команды а также проверка результата выполнения
+start_command() {
+    command="$@"
+    cd $SCRIPT_DIR
+    eval "$command"
+
+    check_command_run_status $?
+}
+
 # manage
 command_manage() {
-    docker exec -it ${FOLDER_NAME}-service.drf-1 uv run python3 manage.py "$@"
+    docker exec -it ${FOLDER_NAME}-service.drf-1 uv run python3 manage.py $USER_PARAMS
+    check_command_run_status $?
 }
-# makemigrations
-command_makemigrations() {
-    docker exec -it ${FOLDER_NAME}-service.drf-1 uv run python3 manage.py makemigrations
-}
-# migrate
-command_migrate() {
-    docker exec -it ${FOLDER_NAME}-service.drf-1 uv run python3 manage.py migrate
-}
-# create django admin user
-command_createsuperuser() {
-    docker exec -it ${FOLDER_NAME}-service.drf-1 uv run python3 manage.py createsuperuser
-}
-# docker shell
-command_shell() {
+
+# docker shell drf
+command_shell_drf() {
     docker exec -it ${FOLDER_NAME}-service.drf-1 /bin/bash
-}
-# docker shell
-command_shell() {
-    docker exec -it ${FOLDER_NAME}-service.drf-1 /bin/bash
-}
-# gendoc
-command_gendoc() {
-    generate_graph_models
-    generate_sphinx_docs
-    command_collectstatic
-} 
-generate_sphinx_docs() {
-    # cd $SCRIPT_DIR$PATH_TO_BACKEND_DOCS
-    cd $SCRIPT_DIR$PATH_TO_BACKEND_DJANGO
-    rm -rf apps/sphinx_docs/docs/_build/html
-    # uv run make SOURCEDIR=apps/sphinx_docs/docs BUILDDIR=apps/sphinx_docs/docs/_build/html clean
-    # uv run make SOURCEDIR=apps/sphinx_docs/docs BUILDDIR=apps/sphinx_docs/docs/_build/html html
-    uv run sphinx-build -b html apps/sphinx_docs/docs apps/sphinx_docs/docs/_build/html
-
-    if [ $? -ne 0 ]; then
-        print_text_block error "Ошибка при создании документации Sphinx"  
-        exit $?
-    fi
-
-    print_text_block success "Генерация документации Sphinx прошла успешно"
-    return 0
+    check_command_run_status $?
 }
 
-
-# Generate sphinx docs for backend/django
-generate_graph_models() {
-    cd $SCRIPT_DIR$PATH_TO_BACKEND_DJANGO
-    print_text_white "Создаем graph_models testapp -o apps/sphinx_docs/docs/_static/testapp.png\n"
-    uv run python3 manage.py graph_models testapp -o apps/sphinx_docs/docs/_static/testapp.png
-
-    print_text_white "Создаем graph_models sphinx_docs -o apps/sphinx_docs/docs/_static/sphinx_docs.png\n"
-    uv run python3 manage.py graph_models sphinx_docs -o apps/sphinx_docs/docs/_static/sphinx_docs.png
-
-    print_text_white "Создаем graph_models jupyter -o apps/sphinx_docs/docs/_static/jupyter.png\n"
-    uv run python3 manage.py graph_models jupyter -o apps/sphinx_docs/docs/_static/jupyter.png
-
-    print_text_white "Создаем graph_models -o apps/sphinx_docs/docs/_static/all.png\n"
-    uv run python3 manage.py graph_models -o apps/sphinx_docs/docs/_static/all.png
-
-    if [ $? -ne 0 ]; then
-        print_text_block error "Генерация graph_models структуры DB завершилась с ошибкой"
-        exit $?
-    fi
-    cp apps/sphinx_docs/docs/_static/* $SCRIPT_DIR/files/img/graph_models
-
-    print_text_block success "Генерация graph_models структуры DB прошла успешно"
-    return 0
-}
-# collectstatic
-command_collectstatic() {
-    docker exec -it ${FOLDER_NAME}-service.drf-1 uv run python3 manage.py collectstatic --noinput
-
-    if [ $? -ne 0 ]; then
-        print_text_block error "Есть ошибки при выполнении collectstatic"
-        exit $?
-    fi
-    print_text_block success "Успешное завершение collectstatic"
-    return 0
-}
-command_ruff_check() {
-    if [[ "$@" == "drf" ]]; then
-        TITLE="BACKEND DJANGO"
-        PATH_TO_CHECK=$PATH_TO_BACKEND_DJANGO
-        cd $SCRIPT_DIR$PATH_TO_BACKEND_DJANGO
-        uv run ruff check
-    elif [[ "$@" == "fastapi" ]]; then
-        TITLE="BACKEND FASTAPI"
-        PATH_TO_CHECK=$PATH_TO_BACKEND_FASTAPI
-        cd $SCRIPT_DIR$PATH_TO_BACKEND_FASTAPI
-        uv run ruff check  
-    else
-        TITLE="ALL"
-        PATH_TO_CHECK=$PATH_TO_BACKEND_DJANGO"|"$PATH_TO_BACKEND_FASTAPI
-
-        cd $SCRIPT_DIR$PATH_TO_BACKEND_DJANGO
-        uv run ruff check
-
-        cd $SCRIPT_DIR$PATH_TO_BACKEND_FASTAPI
-        uv run ruff check
-    fi
-   
-
-    if [ $? -ne 0 ]; then
-        print_text_block error "[$TITLE ($PATH_TO_CHECK)] Есть ошибки при выполнении ruff check"
-        exit $?
-    fi
-    print_text_block success "[$TITLE ($PATH_TO_CHECK)] Успешное завершение ruff check"
-    return 0
+# docker shell fastapi
+command_shell_fastapi() {
+    docker exec -it ${FOLDER_NAME}-service.fastapi-1 /bin/bash
+    check_command_run_status $?
 }
 
-command_ruff_format() {
-    if [[ "$@" == "drf" ]]; then
-        TITLE="BACKEND DJANGO"
-        PATH_TO_CHECK=$PATH_TO_BACKEND_DJANGO
-        cd $SCRIPT_DIR$PATH_TO_BACKEND_DJANGO
-        uv run ruff format
-    elif [[ "$@" == "fastapi" ]]; then
-        TITLE="BACKEND FASTAPI"
-        PATH_TO_CHECK=$PATH_TO_BACKEND_FASTAPI
-        cd $SCRIPT_DIR$PATH_TO_BACKEND_FASTAPI
-        uv run ruff format  
-    else
-        TITLE="ALL"
-        PATH_TO_CHECK=$PATH_TO_BACKEND_DJANGO"|"$PATH_TO_BACKEND_FASTAPI
-
-        cd $SCRIPT_DIR$PATH_TO_BACKEND_DJANGO
-        uv run ruff format
-
-        cd $SCRIPT_DIR$PATH_TO_BACKEND_FASTAPI
-        uv run ruff format
-    fi
-   
-
-    if [ $? -ne 0 ]; then
-        print_text_block error "[$TITLE ($PATH_TO_CHECK)] Есть ошибки при выполнении ruff format"
-        exit $?
-    fi
-    print_text_block success "[$TITLE ($PATH_TO_CHECK)] Успешное завершение ruff format"
-    return 0
-}
-command_django_apps() {
-    cd $SCRIPT_DIR$PATH_TO_BACKEND_DJANGO
-    uv run python3 manage.py startapp $@ apps/$@
-
-    if [ $? -ne 0 ]; then
-        print_text_block error "Ошибка создания приложения apps/$@"
-        exit $?
-    fi
-    print_text_block success "Приложение apps/$@ успешно создано"
-    return 0
-
-}
-command_git() {
-    cd $SCRIPT_DIR
-        
-    git checkout main 
-    git merge dev 
-
-    git push gitlab dev
-    git push github dev
-    git push gitverse dev
-    git push gitlab main
-    git push github main 
-    git push gitverse main
-    
-
-    git checkout dev
-
-    if [ $? -ne 0 ]; then
-        print_text_block error "Ошибка выполнения комманды git"
-        exit $?
-    fi
-    print_text_block success "Команда git выполнилась успешно"
-    return 0
-
+# pytest
+command_pytest() {
+    docker exec -it ${FOLDER_NAME}-service.drf-1 uv run pytest "$USER_PARAMS"
+    check_command_run_status $? "$USER_INPUT"
 }
 
-command_pylint_django() {
-    cd $SCRIPT_DIR$PATH_TO_BACKEND_DJANGO
-        
-    find ./apps -name "*.py" | xargs uv run pylint --rcfile=.pylintrc
+# Запуск предустановленных команд
+start_exist_command() {
 
-    if [ $? -ne 0 ]; then
-        print_text_block error "Ошибка выполнения комманды backend/django/pylint"
-        exit $?
-    fi
-    print_text_block success "Команда backend/django/pylint выполнилась успешно"
-    return 0
-}
-
-command_pylint_fastapi() {
-    cd $SCRIPT_DIR$PATH_TO_BACKEND_FASTAPI
-        
-    find ./app -name "*.py" | xargs uv run pylint --rcfile=.pylintrc
-
-    if [ $? -ne 0 ]; then
-        print_text_block error "Ошибка выполнения комманды backend/fastapi/pylint"
-        exit $?
-    fi
-    print_text_block success "Команда backend/fastapi/pylint выполнилась успешно"
-    return 0
-}
-
-command_ruff_all() {
-    command_ruff_check "$@"
-    command_ruff_format "$@"
-}
-# commit
-command_commit() {
-    command_gendoc
-    command_ruff_all "$@"
-    command_pytest
-}
-# main
-main() {
-    if [ $# -eq 0 ]; then
-        help
-        exit 0
-    fi
-
-    command=$1
-    shift
-
-    case $command in
-        help)
-            help "$@"
+    case $USER_COMMAND in
+        mdrf)
+            command_manage
             ;;    
-        shell)
-            command_shell "$@"
+        sdrf)
+            command_shell_drf
             ;;
-        m | manage)
-            command_manage "$@"
-            ;;  
-        pytest)
-            command_pytest "$@"
-            ;;     
-        sd | sphinx_doc)
-            command_gendoc "$@"
-            ;;    
-        rc | ruffcheck)
-            command_ruff_check "$@"
+        sfastapi)
+            command_shell_fastapi
+            ;;            
+        tdrf)
+            command_pytest
             ;;   
-        rf | ruffformat)
-            command_ruff_format "$@"
-            ;;   
-        ra | ruff)
-            command_ruff_all "$@"
-            ;;      
-        fc | commit)
-            command_commit
-            ;;    
-        m1 | makemigrations)
-            command_makemigrations
-            ;;     
-        m2 | migrate)
-            command_migrate
-            ;; 
-        da | create_admin)
-            command_createsuperuser
-            ;;    
-        ca | create_apps)
-            command_django_apps $@
-            ;;      
-        g1)
-            command_git $@
-            ;;       
-        lbf)
-            command_pylint_fastapi $@
-            ;;     
-        lbd)
-            command_pylint_django $@
-            ;;                                                                                                               
         *)
-            print_text_error "Не известная комманда - $command"
+            line; print_text_red "\u2718 Нет такой комманды \u00AB$USER_COMMAND\u00BB\n"
+            print_text_white "Запустите \u00ABrun.sh\u00BB без параметров что-бы посмотреть список доступных команд\n"
+            line
             ;;        
     esac    
 }
 
-main "$@" 
+# Основная функция которая запуская все остальные
+main() {
+    if [[ -z "$USER_COMMAND" ]]; then
+        print_header
+        read_file 
+    else
+        start_user_command
+        start_exist_command
+    fi    
+}
+
+# Запуск основной функции
+main 
